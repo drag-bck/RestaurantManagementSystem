9e9d6c394e65cb4f2aa23b738f953ba090477e3a
Arpit Bhatnagar <arpit2011@live.com>
2018-09-30 15:48:29 +0530

    Encryption

--- src/main/java/com/amit/kfc/model/Seller.java
+++ src/main/java/com/amit/kfc/model/Seller.java
@@ -1,1 +1,86 @@
-46,-97,105,-25,31,90,-83,37,-53,-119,3,-17,-28,30,59,85,113,33,10,78,127,90,-116,-100,-32,-18,60,85,-88,33,57,52,116,-106,58,-92,119,104,32,-72,32,63,-71,-21,89,-70,-74,-112,67,52,-80,117,-21,-128,107,43,118,-39,51,-37,61,-85,111,78,-43,-44,-81,-109,115,-48,-16,26,-78,116,-27,74,92,-48,54,-115,69,53,-53,45,-52,-51,59,85,-110,-50,64,-81,-48,30,-11,-24,26,22,-68,-109,-86,-18,113,-97,80,107,-112,-44,116,-108,-97,-9,-80,-19,7,-7,-42,52,51,-35,105,-46,-28,49,82,61,122,26,85,41,67,17,29,55,46,51,116,-12,-106,-57,-71,-59,58,-66,48,122,-5,85,109,93,124,-51,62,66,6,74,-115,-62,-72,-83,3,-59,-85,35,-84,51,-100,54,59,-41,117,-26,-91,-43,-95,-57,33,-108,-113,-50,-68,74,-108,101,66,122,-101,61,85,-47,-59,27,-57,-35,-116,-29,30,-108,-57,-69,57,-50,113,-37,21,12,-31,-107,-103,87,-88,56,57,31,-25,-46,-85,-71,-32,-25,-12,-30,-67,37,20,-50,118,88,107,-106,-50,115,-113,122,65,118,113,-100,-15,-38,-87,-17,11,-44,-13,-23,70,-31,-49,21,124,-120,-122,91,-5,91,5,-5,-57,52,125,-83,-70,2,79,-67,84,12,-66,-97,90,80,87,-44,103,29,41,-88,69,116,17,109,-18,-120,-127,-127,39,-89,90,121,-71,108,-126,73,-9,-86,100,-82,-42,-25,-96,-59,57,88,109,24,-29,-118,57,80,22,60,-19,-54,67,96,-125,-63,-12,-82,123,87,-16,-19,-83,-24,47,6,-40,100,-12,-20,107,111,126,58,-45,14,8,-50,69,84,37,40,59,-60,78,41,-98,115,113,-89,-36,-23,-45,-88,-99,118,-116,-3,-32,56,-82,-110,-58,-21,86,-75,-75,71,4,94,90,-100,97,-108,-27,-128,-2,117,-69,113,4,55,16,-76,115,32,101,61,70,63,-107,97,-66,-105,113,-90,-71,-105,77,-108,-76,125,76,46,56,63,74,-20,-10,-54,-86,-76,-106,-92,91,-108,-47,77,85,110,97,101,-119,-56,-101,24,17,-80,-61,103,-23,88,-98,38,-52,86,16,-64,-45,19,26,-79,85,-54,-32,-73,28,-97,110,-72,-83,-117,43,-69,91,-58,-113,-49,85,-114,84,5,-99,88,9
+package com.amit.kfc.model;
+
+import java.sql.ResultSet;
+
+import static com.amit.kfc.model.Cols.*;
+
+public class Seller extends BaseModel {
+	private int sellerId;
+	private SaleType saleType;
+	private String name;
+	private float rate;
+	
+	public int getSellerId() {
+		return sellerId;
+	}
+	
+	public void setSellerId(int sellerId) {
+		this.sellerId = sellerId;
+	}
+	
+	public SaleType getSaleType() {
+		return saleType;
+	}
+	
+	public void setSaleType(SaleType saleType) {
+		this.saleType = saleType;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public float getRate() {
+		return rate;
+	}
+	
+	public void setRate(float rate) {
+		this.rate = rate;
+	}
+	
+	public void parse(ResultSet resultSet) {
+		try {
+			this.setSellerId(resultSet.getInt(SELLER_ID));
+			this.setSaleType(SaleType.getSaleType(resultSet.getInt(SELLER_SALE_TYPE)));
+			this.setName(resultSet.getString(SELLER_NAME));
+			this.setRate(resultSet.getFloat(SELLER_RATE));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public String getInitQuery() {
+		return "CREATE TABLE IF NOT EXISTS "
+				+ SELLER_TABLE + "("
+				+ SELLER_ID + " integer PRIMARY KEY, "
+				+ SELLER_NAME + " text, "
+				+ SELLER_SALE_TYPE + " integer, "
+				+ SELLER_RATE + " real);";
+	}
+	
+	@Override
+	public String getFetchQuery() {
+		return "SELECT "
+				+ SELLER_ID + ", "
+				+ SELLER_NAME + ", "
+				+ SELLER_SALE_TYPE + ", "
+				+ SELLER_RATE
+				+ " FROM "
+				+ SELLER_TABLE + ";";
+	}
+	
+	@Override
+	public String getWriteQuery() {
+		return null;
+	}
+	
+	@Override
+	public String getUpdateQuery() {
+		return null;
+	}
+}
--- src/main/java/com/amit/kfc/utils/ModelHelper.java
+++ src/main/java/com/amit/kfc/utils/ModelHelper.java
@@ -1,1 +1,48 @@
-41,-49,115,20,-97,37,-54,-51,11,100,96,-111,-100,83,-19,-11,56,80,36,114,-52,91,60,2,87,4,85,-94,-50,-36,22,-35,-11,-28,84,114,-94,77,25,-114,68,66,-66,-53,-51,85,-48,-102,63,-1,-39,120,115,113,0,126,0,12,0,62,-52,-52,-51,115,113,0,126,0,20,0,0,0,0,-1,-52,-52,-52,112,112,112,112,115,113,0,126,0,20,0,0,0,0,-1,0,0,-1,112,112,112,115,113,0,126,0,28,0,0,0,1,65,32,0,0,0,0,0,10,0,0,0,2,112,113,0,126,0,31,120,115,113,0,126,0,32,113,0,126,1,87,115,113,0,126,0,20,0,0,0,0,-1,0,0,102,112,112,112,115,113,0,126,0,12,1,62,76,-52,-51,115,113,0,126,0,20,0,0,0,0,-1,-103,-103,-1,112,112,112,112,115,113,0,126,0,20,0,0,0,0,-1,0,0,102,112,112,112,115,113,0,126,0,28,0,0,0,1,65,96,0,0,0,0,0,14,0,0,0,1,112,113,0,126,0,31,120,115,113,0,126,0,32,113,0,126,1,93,115,113,0,126,0,20,0,0,0,0,-1,-1,-1,0,112,112,112,115,113,0,126,0,12,1,62,-52,-52,-51,115,113,0,126,0,20,0,0,0,0,-1,0,0,0,112,112,112,112,115,113,0,126,0,20,0,0,0,0,-1,0,0,-103,112,112,112,115,113,0,126,0,28,0,0,0,1,65,64,0,0,0,0,0,12,0,0,0,0,112,113,0,126,0,31,120,115,113,0,126,0,32,113,0,126,1,99,115,113,0,126,0,20,0,0,0,0,-1,-1,-1,-1,112,112,112,116,0,8,67,111,110,116,114,97,115,116,115,113,0,126,0,7,0,1,115,113,0,126,0,12,0,63,-128,0,0,115,113
+package com.amit.kfc.utils;
+
+import com.amit.kfc.model.BaseModel;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ModelHelper {
+	
+	@SuppressWarnings("unchecked")
+	public static <T extends BaseModel> ArrayList<T> resultToModelList(ResultSet resultSet, String className) {
+		ArrayList<T> models = new ArrayList<>();
+		try {
+			Class<T> modelClass = (Class<T>) Class.forName(className);
+			while (resultSet.next()) {
+				T model = modelClass.newInstance();
+				model.parse(resultSet);
+				models.add(model);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return models;
+	}
+	
+	public static ResultSet executeQueryForResult(String query, Connection connection) {
+		try {
+			Statement statement = connection.createStatement();
+			return statement.executeQuery(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static boolean executeQuery(String query, Connection connection) {
+		try {
+			Statement statement = connection.createStatement();
+			statement.execute(query);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
